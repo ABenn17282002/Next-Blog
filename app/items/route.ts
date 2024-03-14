c6c32f7760ec8aac9ed59a68ba41409ba185b9fc
ABenn17282002 <a.benn17282002@gmail.com>
2024-03-14 07:25:33 +0000

    OpenAIChatErrror修正

--- app/items/route.ts
+++ app/items/route.ts
@@ -1,5 +1,12 @@
 // Revalidating Cached Data
+export const dynamic = "auto";
+export const dynamicParams = true;
+export const revalidate = false;
+export const fetchCache = "auto";
+export const runtime = "nodejs";
+export const preferredRegion = "auto";
 
+// GETメソッド
 export async function GET() {
   //データの取得
   const res = await fetch("https://data.mongodb-api.com/...", {
@@ -10,4 +17,12 @@ export async function GET() {
   const data = await res.json();
 
   return Response.json(data);
+}
+
+// POSTメソッド
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const name = formData.get("name");
+  const email = formData.get("email");
+  return Response.json({ name, email });
 }
