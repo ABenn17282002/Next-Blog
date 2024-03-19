eb61bbe0893d417be27b82bf9c46c035a73a7cbc
ABenn17282002 <a.benn17282002@gmail.com>
2024-03-19 05:34:31 +0000

    Linking and Navigating

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,8 +7,14 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello, Next.js!</h1>
+    <main className="py-2 px-4">
+      <h1 className="mb-4">Hello, Next.js!</h1>
+      <Link
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        href="/dashboard"
+      >
+        Dashboard
+      </Link>
     </main>
   );
 }
