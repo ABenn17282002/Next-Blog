91903504dcc9c3b9832482bd5d5e975594d3b3ad
ABenn17282002 <a.benn17282002@gmail.com>
2024-03-25 14:07:06 +0000

    Loading UI and Streaming

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,17 @@
-// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
-export default function Page() {
-  return <h1>Hello, Dashboard Page!</h1>;
+import { Suspense } from "react";
+// PostFeed,Weatherコンポーネントの導入
+import PostFeed from "../components/PostFeed";
+import Weather from "../components/Weather";
+
+export default function Posts() {
+  return (
+    <section>
+      <Suspense fallback={<p>Loading feed...</p>}>
+        <PostFeed />
+      </Suspense>
+      <Suspense fallback={<p>Loading weather...</p>}>
+        <Weather />
+      </Suspense>
+    </section>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // imageの読み込み
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
